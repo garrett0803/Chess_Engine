ab3ae2bd92fb83b0dae10812c40bd35eb376dce6
Garrett Welsch <42852431+garrett0803@users.noreply.github.com>
2022-05-02 10:55:15 -0500

    Create King.java

---
+++ src/King.java
@@ -0,0 +1,14 @@
+public class King {
+    private int[] location;
+
+    public int[] getMove(){
+
+    }
+
+    public void setLoc(int[] location){
+        this.location=location;
+    }
+     public int[] getLoc(){
+         return location;
+     }
+}
